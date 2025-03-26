45713dc8b8b937f73f309ae1a50731f39b3abb69
JuanK31050809 <juanklolo31@gmail.com>
2025-03-25 22:51:52 -0500

    Actualización mensaje más preciso

--- csc-1.py
+++ csc-1.py
@@ -1,10 +1,10 @@
 import math 
 
-x = float(input("Entrada: "))
+x = float(input("Ingresa el valor para calcular "))
 
 if abs(x) < 1:
     print("Cosecante a la -1 solo está definida para |x| ≥ 1")
 else:
     angulo = math.asin(1 / x)
-    print(f"csc⁻¹({x}) = {angulo} radianes")
+    print(f"csc⁻¹({x}) = {angulo} en radianes")
     
